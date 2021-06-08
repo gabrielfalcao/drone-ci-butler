1cfa0705ace3dc0d34f1ce960c7484cbe3e32c63
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-06-08 16:16:58 +0200

    react app

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+/* import "@nyt-cms/ink/tokens/typography/fonts"; */
+import {
+  Button,
+  Dialog,
+  colors,
+  spacing,
+  stacking,
+  typography,
+} from "@nyt-cms/ink";
+
+function App() {
+  return (
+    <>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <main>
+        <p>
+          <strong>Hello!</strong>
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
